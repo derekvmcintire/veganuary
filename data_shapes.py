4ec2820a9755fb98f2658add143e8a73050f9397
Derek McIntire <derek@Dereks-MBP.fios-router.home>
2021-01-10 21:07:04 -0500

    Mock out rest of class

---
+++ data_shapes.py
@@ -0,0 +1,62 @@
+RIDERS_SHAPE = {
+    "a": [],
+    "b": [],
+    "c": [],
+    "d": []
+}
+REGISTERED_ZIDS_SHAPE = {
+    "a": [],
+    "b": [],
+    "c": [],
+    "d": []
+}
+STAGE_RESULTS_SHAPE = {
+    "a": [],
+    "b": [],
+    "c": [],
+    "d": []
+}
+FILTERED_STAGE_RESULTS_SHAPE = {
+    "a": [],
+    "b": [],
+    "c": [],
+    "d": []
+}
+OVERALL_RSULTS_SHAPE = {
+    "a": {
+        "individual_gc": [],
+        "womens_gc": [],
+        "womens_sprint": [],
+        "qom": [],
+        "team_gc": [],
+        "sprint": [],
+        "kom": []
+    },
+    "b": {
+        "individual_gc": [],
+        "womens_gc": [],
+        "womens_sprint": [],
+        "qom": [],
+        "team_gc": [],
+        "sprint": [],
+        "kom": []
+    },
+    "c": {
+        "individual_gc": [],
+        "womens_gc": [],
+        "womens_sprint": [],
+        "qom": [],
+        "team_gc": [],
+        "sprint": [],
+        "kom": []
+    },
+    "d": {
+        "individual_gc": [],
+        "womens_gc": [],
+        "womens_sprint": [],
+        "qom": [],
+        "team_gc": [],
+        "sprint": [],
+        "kom": []
+    },
+}
