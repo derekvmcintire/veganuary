79490f9c56e098e12c5c2e3756e68853d7b146b5
Derek McIntire <derek@Dereks-MBP.fios-router.home>
2021-01-11 08:33:03 -0500

    Update data

--- get_results.py
+++ get_results.py
@@ -2,7 +2,12 @@ from veganuary import CalculateResults
 import stage_2_rider_data
 
 # The current data doesn't include filtered data, nor doesit include sprint/KOM data
-# c = CalculateResults(stage_2_rider_data.STAGE_2_RESULTS)
-# print("Number of A Riders:", len(c.riders["a"]))
-# print("Number of A Rider Rsults:", len(c.stage_results["a"]))
-print("This aint gonna work until we get those ZIDs")
+c = CalculateResults(stage_2_rider_data.STAGE_2_RESULTS)
+print("Number of A Riders:", len(c.riders["a"]))
+print("Number of valid A Rider Rsults:", len(c.stage_results["a"]))
+print("Number of B Riders:", len(c.riders["b"]))
+print("Number of valid B Rider Rsults:", len(c.stage_results["b"]))
+print("Number of C Riders:", len(c.riders["c"]))
+print("Number of valid C Rider Rsults:", len(c.stage_results["c"]))
+print("Number of D Riders:", len(c.riders["d"]))
+print("Number of valid D Rider Rsults:", len(c.stage_results["d"]))
